c06fcf6b5f9e82e9b382342b7d47f5221a398c21
assanalibatyrkhan <asankek12@gmail.com>
2024-08-08 16:04:31 +0500

    init commit

---
+++ api/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.routers import auth, workouts, routines
+
+from api.database import Base, engine
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://vercel-deployment-client-xi.vercel.app"
+                   ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+app.include_router(router=auth.router)
+app.include_router(router=workouts.router)
+app.include_router(router=routines.router)
